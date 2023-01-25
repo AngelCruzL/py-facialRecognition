58bcc87dd641cd39ac75bd8f64e8812b2538e6a8
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-01-24 23:42:41 -0600

    fix: rename file to model_training.py

--- model_training.py
+++ model_training.py
@@ -5,7 +5,6 @@ import numpy as np
 
 data_path = os.path.join(os.getcwd(), 'data')
 people_list = os.listdir(data_path)
-# print(people_list)
 
 labels = []
 face_samples = []
@@ -13,16 +12,11 @@ label = 0
 
 for name_dir in people_list:
     person_path = os.path.join(data_path, name_dir)
-    # print('Leyendo las imágenes')
 
     for filename in os.listdir(person_path):
-        # print('Rostros: ', name_dir + '/' + filename)
         filename_path = os.path.join(person_path, filename)
         labels.append(label)
         face_samples.append(cv2.imread(os.path.join(filename_path), 0))
-        # image = cv2.imread(filename_path, 0)
-        # cv2.imshow('image', image)
-        # cv2.waitKey(10)
 
     label = label + 1
 
