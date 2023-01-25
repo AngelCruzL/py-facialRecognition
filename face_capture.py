1e30bc9085593b8635ac60776589cfe99f85bd1f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-01-24 21:53:46 -0600

    feat: add face capture script

---
+++ face_capture.py
@@ -0,0 +1,46 @@
+import os
+
+import cv2
+import imutils
+
+person_name = 'luis'
+
+data_path = os.path.join(os.getcwd(), 'data')
+person_path = os.path.join(data_path, person_name)
+
+if not os.path.exists(person_path):
+    os.mkdir(person_path)
+    print('Carpeta creada: ', person_path)
+
+capture = cv2.VideoCapture('assets/videos/test-1.mp4')
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+count = 0
+
+while True:
+    ret, frame = capture.read()
+
+    if ret == False:
+        break
+
+    frame = imutils.resize(frame, width=640)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aux_frame = frame.copy()
+
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        rostro = aux_frame[y:y+h, x:x+w]
+        rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(person_path + f'/rostro_{count}.jpg', rostro)
+        count = count + 1
+
+    cv2.imshow('frame', frame)
+
+    k = cv2.waitKey(1)
+    if k == 27 or count >= 300:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
